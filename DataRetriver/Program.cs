c5f3fed5f5123900febcac34abddac7b450af5ff
Sana1007 <oksana.medvid14@gmail.com>
2019-05-04 16:43:02 +0300

    Add project files.

---
+++ DataRetriver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataRetriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
